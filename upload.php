fae68268aa90c793c7ee0b58af30e03ab848560b
Lakpa Ringi Sherpa <devilsground@gmail.com>
2019-12-15 16:55:36 +0545

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,7 +2,7 @@
 	include 'dbconfig.php';
 
 	$upload_path='image/';
-	$upload_url='http://localhost/teaching/Webapi/'.$upload_path;
+	$upload_url='http://192.168.214.2/teaching/Webapi/'.$upload_path;
 
 
 	//var_dump($upload_url);die();test
@@ -11,25 +11,25 @@
 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 
-		if (isset($_POST['name']) and isset($_POST['email']) and isset($_FILES['image']['name'])) {
+		if (isset($_POST['name']) and isset($_POST['description']) and isset($_FILES['image']['name'])) {
 			$name=$_POST['name'];
-			$email=$_POST['email'];
+			$description=$_POST['description'];
 			$fileinfo=pathinfo($_FILES['image']['name']);
 			$extension = $fileinfo['extension'];
-			$file_url = $upload_url.'IMG_'.$name.'.'.$extension;
-			$file_path= $upload_path.'IMG_'.$name.'.'.$extension;
+			$file_url = $upload_path . getFileName() . '.'. $extension; 
+			$file_path= $upload_path . getFileName() . '.'. $extension; 
 
 			//var_dump($file_path);die();
 
 			try {
 				move_uploaded_file($_FILES['image']['tmp_name'], $file_path);
 
-				$sql = "INSERT INTO `user` (`name`,`email`,`photo`) VALUES ('$name','$email','$file_url')";
+				$sql = "INSERT INTO `user` (`name`,`description`,`photo`) VALUES ('$name','$description','$file_url')";
 
 				if(mysqli_query($conn,$sql)){
 					$response['error']=false;
 					$response['name']=$name;
-					$response['email']=$email;
+					$response['description']=$description;
 				}
 			} catch (Exception $e) {
 					$response['error']=true;
@@ -42,5 +42,21 @@
 		}
 	}else{
 		echo "else condition";
+	}
+	
+	function getFileName(){
+		$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "university";
+		$conn = mysqli_connect($servername, $username, $password, $dbname) or die('Unable to Connect...');
+		$sql = "SELECT max(id) as id FROM user";
+		$result = mysqli_fetch_array(mysqli_query($conn,$sql));
+		
+		mysqli_close($conn);
+		if($result['id']==null)
+			return 1; 
+		else 
+			return ++$result['id']; 
 	}
 ?>
