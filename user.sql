58b61fb740e701defd27df46cdc30650e1dd9e9a
Lakpa Ringi Sherpa <devilsground@gmail.com>
2019-12-25 16:15:29 +0545

    Update user.sql

--- user.sql
+++ user.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 15, 2019 at 11:11 AM
+-- Generation Time: Dec 25, 2019 at 10:28 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.1.29
 
@@ -35,15 +35,21 @@ CREATE TABLE IF NOT EXISTS `user` (
   `description` text COLLATE utf8mb4_unicode_ci,
   `photo` varchar(550) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `description`, `photo`) VALUES
-(4, 'test', 'test@test.com', 'http://localhost/teaching/Webapi/image/IMG_test.jpg'),
-(5, 'test', 'this is just a test', 'http://localhost/teaching/Webapi/image/IMG_test.jpg');
+(22, 'Batman', 'Batman is a fictional superhero appearing in American comics books published by DC Comics.', 'image/Batman.jpg'),
+(21, 'Iron Man', 'Iron Man is a fictional American hero that is published by Marvel comics', 'image/Iron Man.jpg'),
+(23, 'Spider Man', 'Spider-Man is a fictional superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy #15 (August 1962) in the Silver Age of Comic Books', 'image/spiderman.jpg'),
+(24, 'The Hulk', 'The Hulk is a fictional superhero appearing in publications by the American publisher Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, the character first appeared in the debut issue of The Incredible Hulk', 'image/hulk.jpg'),
+(25, 'Thor', 'Thor Odinson is a fictional superhero appearing in American comic books published by Marvel Comics. The character, which is based on the Norse deity of the same name, is the Asgardian god of thunder ', 'image/thor.jpg'),
+(26, 'Wonder Woman', 'Wonder Woman is a fictional superheroine, appearing in American comic books published by DC Comics. The character is a founding member of the Justice League.', 'image/wonderwoman.jpg'),
+(27, 'Green Arrow', 'Green Arrow is a fictional superhero who appears in comic books published by DC Comics. Created by Mort Weisinger and designed by George Papp', 'image/greenarrow.jpg'),
+(28, 'Superman', 'Superman is a fictional superhero. The character was created by writer Jerry Siegel and artist Joe Shuster, and first appeared in Action Comics #1', 'image/superman.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
