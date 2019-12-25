d50ddb60357f876374c5d8fee5eab0bff6d48071
Lakpa Ringi Sherpa <devilsground@gmail.com>
2019-12-25 16:16:29 +0545

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,62 +1,22 @@
 <?php
-	include 'dbconfig.php';
+	require 'dbconfig.php';
 
-	$upload_path='image/';
-	$upload_url='http://192.168.214.2/teaching/Webapi/'.$upload_path;
-
-
-	//var_dump($upload_url);die();test
-
-	$response = array();
-
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-
-		if (isset($_POST['name']) and isset($_POST['description']) and isset($_FILES['image']['name'])) {
-			$name=$_POST['name'];
-			$description=$_POST['description'];
-			$fileinfo=pathinfo($_FILES['image']['name']);
-			$extension = $fileinfo['extension'];
-			$file_url = $upload_path . getFileName() . '.'. $extension; 
-			$file_path= $upload_path . getFileName() . '.'. $extension; 
-
-			//var_dump($file_path);die();
-
-			try {
-				move_uploaded_file($_FILES['image']['tmp_name'], $file_path);
-
-				$sql = "INSERT INTO `user` (`name`,`description`,`photo`) VALUES ('$name','$description','$file_url')";
-
-				if(mysqli_query($conn,$sql)){
-					$response['error']=false;
-					$response['name']=$name;
-					$response['description']=$description;
-				}
-			} catch (Exception $e) {
-					$response['error']=true;
-					$response['message']=$e->getMessage();
-			}
-
-			echo json_encode($response);
-			mysqli_close($conn);
-
+	if($conn){
+		$name=$_POST['name'];
+		$description=$_POST['description'];
+		$image = $_POST['image'];
+		$upload_path = 'image/'.$name.'.jpg';
+		
+		$sql = "INSERT INTO `user` (`name`,`description`,`photo`) VALUES ('$name','$description','$upload_path')";
+		
+		if(mysqli_query($conn,$sql)){
+			file_put_contents($upload_path,base64_decode($image));
+			echo json_encode(array('response'=>'Image uploaded successfully..'));
+		}else{
+			echo json_encode(array('response'=>'Image upload failed...'));
 		}
-	}else{
-		echo "else condition";
-	}
-	
-	function getFileName(){
-		$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "university";
-		$conn = mysqli_connect($servername, $username, $password, $dbname) or die('Unable to Connect...');
-		$sql = "SELECT max(id) as id FROM user";
-		$result = mysqli_fetch_array(mysqli_query($conn,$sql));
 		
 		mysqli_close($conn);
-		if($result['id']==null)
-			return 1; 
-		else 
-			return ++$result['id']; 
+		
 	}
 ?>
